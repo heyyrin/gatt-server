5bda6af423b39cac6da22efb82990e11083e0d0b
kimhyerin <heyyrin@gmail.com>
2020-11-09 17:48:53 +0900

    RX 전송 부분 수정

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/paypal/gatt"
 )
 
-// example
+// sample service
 func NewCountService() *gatt.Service {
 	n := 0
 	s := gatt.NewService(gatt.MustParseUUID("09fc95c0-c111-11e3-9904-0002a5d5c51b"))
@@ -39,31 +39,71 @@ func NewCountService() *gatt.Service {
 	return s
 }
 
-// 6E400001-B5A3-F393-E0A9-E50E24DCCA9E for the Service
-// 6E400002-B5A3-F393-E0A9-E50E24DCCA9E for the RX Characteristic (Property = Notify)
-// 6E400003-B5A3-F393-E0A9-E50E24DCCA9E for the TX Characteristic (Property = Write without response)
-func NewTestService() *gatt.Service {
-	//n := 0
-	s := gatt.NewService(gatt.MustParseUUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E"))
+var UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
+var UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // RX Characteristic (Property = Notify)
+var UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // TX Characteristic (Property = Write without response)
 
-	//s.AddCharacteristic(gatt.MustParseUUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")).HandleReadFunc(
-	//	func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
-	//		fmt.Fprintf(rsp, "count: %d", n)
-	//		n++
-	//	})
+func NewUartService() *gatt.Service {
 
-	s.AddCharacteristic(gatt.MustParseUUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E")).HandleNotifyFunc(
-		func(r gatt.Request, n gatt.Notifier) { // no
-			fmt.Fprintf(n, "Count: %d", n.Cap())
+	s := gatt.NewService(gatt.MustParseUUID(UART_SERVICE_UUID))
+	s.AddCharacteristic(gatt.MustParseUUID(UART_RX_CHAR_UUID)).HandleNotifyFunc(
+		func(r gatt.Request, n gatt.Notifier) {
+			cnt := 0
+			for !n.Done() { // notif
+				// 0과 1을 반복해서 데이터를 전송
+				if cnt%2 == 0 {
+					data := "1"
+					sentLen, err := n.Write([]byte(data))
+					if err != nil {
+						log.Println(err)
+					} else {
+						log.Println("Send:", data, " Length :", sentLen)
+					}
+				} else {
+					data := "0"
+					sentLen, err := n.Write([]byte(data))
+					if err != nil {
+						log.Println(err)
+					} else {
+						log.Println("Send:", data, " Length :", sentLen)
+					}
+				}
+				time.Sleep(time.Second)
+				cnt++
+			}
 		})
 
-	s.AddCharacteristic(gatt.MustParseUUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E")).HandleWriteFunc(
+	s.AddCharacteristic(gatt.MustParseUUID(UART_TX_CHAR_UUID)).HandleWriteFunc(
 		func(r gatt.Request, data []byte) (status byte) {
 			log.Println("Wrote:", string(data))
 			return gatt.StatusSuccess
 		})
 
 	return s
+}
+
+func onStateChanged(gattServer gatt.Device, s gatt.State) {
+
+	fmt.Printf("State: %s\n", s)
+	switch s {
+	case gatt.StatePoweredOn:
+		// Setup GAP and GATT services
+		gattServer.AddService(service.NewGapService("HYERIN001")) // 'Generic Access' Service
+		gattServer.AddService(service.NewGattService())           // 'Generic Attribute' Service
+
+		// add uart service
+		uartService := NewUartService()
+		gattServer.AddService(uartService)
+
+		// Advertise device name and service's UUIDs.
+		gattServer.AdvertiseNameAndServices("HYERIN001", []gatt.UUID{uartService.UUID()}) //
+
+		//gattServer.AdvertiseIBeacon(gatt.MustParseUUID("AA6062F098CA42118EC4193EB73CCEB6"), 1, 2, -59)  // beacon code!
+
+	default:
+		fmt.Print("default????")
+	}
+
 }
 
 func main() {
@@ -81,27 +121,6 @@ func main() {
 			fmt.Println(c.MTU())
 		}),
 	)
-
-	onStateChanged := func(d gatt.Device, s gatt.State) {
-		fmt.Printf("State: %s\n", s)
-		switch s {
-		case gatt.StatePoweredOn:
-			// Setup GAP and GATT services
-			gattServer.AddService(service.NewGapService("HYERIN001"))
-			//gattServer.AddService(service.NewGattService())
-
-			// add service
-			testService := NewTestService()
-			gattServer.AddService(testService)
-
-			// Advertise device name and service's UUIDs.
-			gattServer.AdvertiseNameAndServices("HYERIN001", []gatt.UUID{testService.UUID()}) // 해당 코드 있어야함
-
-			//gattServer.AdvertiseIBeacon(gatt.MustParseUUID("AA6062F098CA42118EC4193EB73CCEB6"), 1, 2, -59)  // beacon code
-		default:
-			fmt.Print("default????")
-		}
-	}
 
 	gattServer.Init(onStateChanged)
 
